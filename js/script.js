af37f5753fc9287732eb64f831f06dcac66f4893
Candy Frederiksen <candic3@pdx.edu>
2020-05-24 01:09:37 -0700

    Whole bunch of stuff

--- js/script.js
+++ js/script.js
@@ -34,61 +34,50 @@ $(document).ready(function(){
 });
 
 
-var track = document.getElementById('static');
 
-var controlBtn = document.getElementById('play');
 
-function playPause() {
-    if (track.paused) {
-        track.play();
-        //controlBtn.textContent = "Pause";
-        controlBtn.className = "pause";
-    } else { 
-        track.pause();
-         //controlBtn.textContent = "Play";
-        controlBtn.className = "play";
-    }
-}
 
-controlBtn.addEventListener("click", playPause);
-track.addEventListener("ended", function() {
-  controlBtn.className = "play";
+$(document).ready(function(){
+  $(".grid1").hide();
+  $(".on").click(function(){
+    $(".grid1").toggle(1000);
+  });
 });
 
+$(document).ready(function(){
+  $(".grid2").hide();
+  $(".record").click(function(){
+    $(".grid2").toggle(1000);
+  });
+});
 
-    $(document).ready(function(){
-        $('#show').hide()
-        $('#click').on('click', function () {
-            $('#show').toggle(1000);
-        });
-    });
+$(document).ready(function(){
+  $(".grid3").hide();
+  $(".label").click(function(){
+    $(".grid3").toggle(1000);
+  });
+});
 
+$(document).ready(function(){
+  $(".grid4").hide();
+  $(".cartridge").click(function(){
+    $(".grid4").toggle(1000);
+  });
+});
 
+$(document).ready(function(){
+  $(".grid5").hide();
+  $(".arm").click(function(){
+    $(".grid5").toggle(1000);
+  });
+});
 
-    $(document).ready(function(){
-        $('#show_2').hide()
-        $('#click_2').on('click', function () {
-            $('#show_2').toggle(1000);
-        });
-    });
-
-
-
-    $(document).ready(function(){
-        $('#show_3').hide()
-        $('#click_3').on('click', function () {
-            $('#show_3').toggle(1000);
-        });
-    });
-
-
-
-    $(document).ready(function(){
-        $('#show_4').hide()
-        $('#click_4').on('click', function () {
-            $('#show_4').toggle(1000);
-        });
-    });
+$(document).ready(function(){
+  $(".grid6").hide();
+  $(".center").click(function(){
+    $(".grid6").toggle(1000);
+  });
+});
 
 
 
